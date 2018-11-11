e71d18764e7a0aeb2c3f7e5aa47e8abe7db5d850
JustinHeidema <jrheidema@gmail.com>
2018-11-10 23:39:18 -0800

    Update startcontainer.sh

--- appspec/startcontainer.sh
+++ appspec/startcontainer.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 $(aws ecr get-login --no-include-email --region us-west-2)
-docker pull 642751708823.dkr.ecr.us-west-2.amazonaws.com/justinheidema/pywebapp || {
+docker pull 642751708823.dkr.ecr.us-west-2.amazonaws.com/justinheidema/pywebapp:latest || {
     echo "ERROR: docker pull failed. Sleeping for 10 minutes to allow investigation..."
     sleep 600
     exit 1
 }
-docker run --name pywebapp -p 80:8080 --detach 642751708823.dkr.ecr.us-west-2.amazonaws.com/justinheidema/pywebapp
+docker run --name pywebapp -p 80:8080 --detach 642751708823.dkr.ecr.us-west-2.amazonaws.com/justinheidema/pywebapp:latest
